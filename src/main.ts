9587a3864c9c26dbeb4d496cd799bbbd9cd6d199
RobsonFeitosa <robson.gw@hotmail.com>
2024-03-19 01:17:16 -0300

    Chore: config dotenv; validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import { config } from 'dotenv';
-config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  console.log(process.env.DB_MYSQL_USERNAME);
+  console.log(process.env.DB_MYSQL_PASSWORD);
+  console.log(process.env.DB_MYSQL_DATABASE);
+
   await app.listen(3000);
 }
 bootstrap();
